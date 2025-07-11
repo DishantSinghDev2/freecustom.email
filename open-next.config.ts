ca322ab09a50787bb67eb8249efdf2f4a7c7cdee
Dishant Singh <dishupandey57@gmail.com>
2025-07-11 23:23:30 +0530

    Removed uneccessary files from the git

--- open-next.config.ts
+++
@@ -1,7 +0,0 @@
-// cloudflare.config.ts
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
-
-export default defineCloudflareConfig({
-  incrementalCache: r2IncrementalCache,
-});
